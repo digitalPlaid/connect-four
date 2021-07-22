1b88dd6f42b514bd0a43058da9b112980e519dce
Dan St. Aubin <staubindj@gmail.com>
2021-07-21 21:24:06 -0500

    first commit, adding table, basic game skeleton

---
+++ connectFour.js
@@ -0,0 +1,23 @@
+// check ready
+// add in game board to save myself some typing
+// create board (function)
+// allow player to make a move if it isn't blank
+
+
+$(document).ready(game);
+
+function game() {
+    $('#newGame').on('click', createBoard);
+    let winner = false;
+    let player = 0;
+    // put the restart button on click thing here or in while loop?
+    while (!winner) {
+        // on column clicked, make lowest empty slow the color
+        // switch player and color;
+        // check for win
+    }
+}
+
+function createBoard() {
+
+}
